d35cee20eb17b6e20aa8543d2ba4eae3cd5773e8
Vincent <824649678@qq.com>
2019-07-01 20:32:16 +0800

    correct 7

--- merge_sort/merge_sort.cu
+++ merge_sort/merge_sort.cu
@@ -119,8 +119,9 @@ int main(void)
         blocks /= 2;
         sortedsize *= 2;
     }
-    cudaDeviceSynchronize();
+    CUDA_CHECK(cudaDeviceSynchronize());
     CUDA_CHECK(cudaMemcpy((void*)hC, (void*)dA, N * sizeof(int), cudaMemcpyDeviceToHost));
+
     
     gettimeofday(&t2, NULL);
     printf("GPU merge sort: %g seconds\n", t2.tv_sec - t1.tv_sec + (t2.tv_usec - t1.tv_usec) / 1.0e6);
